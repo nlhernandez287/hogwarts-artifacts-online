75981474ed66cb9e55aea6c579f41597bad772c7
Nathanial Hernandez <n.l.hernandez@tcu.edu>
2024-04-17 14:05:50 -0500

    Refactor

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardServiceTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardServiceTest.java
@@ -1,5 +1,6 @@
 package edu.tcu.cs.hogwartsartifactsonline.wizard;
 
+import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -100,7 +101,7 @@ public class WizardServiceTest {
 
         // Then
         assertThat(thrown)
-                .isInstanceOf(WizardNotFoundException.class)
+                .isInstanceOf(ObjectNotFoundException.class)
                 .hasMessage("Could not find wizard with Id 1:(");
         verify(this.wizardRepository, times(1)).findById(1);
     }
@@ -153,7 +154,7 @@ public class WizardServiceTest {
         given(this.wizardRepository.findById(1)).willReturn(Optional.empty());
 
         // When
-        assertThrows(WizardNotFoundException.class, () -> {
+        assertThrows(ObjectNotFoundException.class, () -> {
             this.wizardService.update(1, update);
         });
 
@@ -184,7 +185,7 @@ public class WizardServiceTest {
         given(this.wizardRepository.findById(1)).willReturn(Optional.empty());
 
         // When
-        assertThrows(WizardNotFoundException.class, () -> {
+        assertThrows(ObjectNotFoundException.class, () -> {
             this.wizardService.delete(1);
         });
 
