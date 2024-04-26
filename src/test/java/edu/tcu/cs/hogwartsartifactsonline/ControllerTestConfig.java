6043e9b7fc2f92c00baa847d816e1dda8738c8f3
Nathanial Hernandez <n.l.hernandez@tcu.edu>
2024-04-26 14:57:51 -0500

    Connect to azure database for mysql with Azure Key Vault

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/ControllerTestConfig.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/ControllerTestConfig.java
@@ -1,13 +1,11 @@
 package edu.tcu.cs.hogwartsartifactsonline;
 
-import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-@Tag("integration")
-@ActiveProfiles("dev")
-public class IntegrationTest {
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
+public class ControllerTestConfig {
 }
